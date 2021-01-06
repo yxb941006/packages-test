25e30657913f79e7c741efb914b5269040dfc799
yinxiaobin <yxb941006@163.com>
2021-01-06 18:07:36 +0800

    chore: test multi packages

--- multi-package-b/index.js
+++ multi-package-b/index.js
@@ -1,9 +1,9 @@
-var test = 1;
+var test = 'multi-package-b';
 console.log('test from packages-test :>> ', test);
 
 const Test = {
     init() {
-        console.log('Test packages is ready!');
+        console.log(test + 'is ready!');
     }
 };
 
